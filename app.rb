5f60fb7f65169eceb356bc2fe4d0f3032c44668b
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-24 22:25:49 +0000

    added random number

--- app.rb
+++ app.rb
@@ -26,5 +26,28 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f
+  @years = params.fetch("user_years").to_i
+  @pv = params.fetch("user_pv").to_i
+
+  @rate = @apr / 100
+  @num_period = 12 * @years
+  @payment_upper = @rate * @pv
+  @payment_lower = 1- ((1+@rate) ** (@num_period * (-1)))
+  @payment = @payment_upper / @payment_lower
+
+
   erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @random = rand(@min..@max)
+
+  erb(:random_results)
 end
