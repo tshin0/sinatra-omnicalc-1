18530dff8f2176500eb43fd50e4b4c8b982c919a
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-24 22:01:51 +0000

    added payment

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new_square_calc)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_sqrt_calc)
+end
+
+get("/square_root/results") do
+  erb(:sqrt_results)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
 end
